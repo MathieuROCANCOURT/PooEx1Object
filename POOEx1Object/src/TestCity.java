ccda60aafdb0381bcb50994aed2b44b10e6eda93
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-17 11:53:01 +0200

    Add Polymorphism test.

--- POOEx1Object/src/TestCity.java
+++ POOEx1Object/src/TestCity.java
@@ -6,13 +6,26 @@ public class TestCity {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		City toulouse = new City("Toulouse", "France", -200);
+		City toulouse = new City("Toulouse", "France", 200);
 
 		System.out.println(toulouse.getNbInhabitants());
-		toulouse.setNbInhabitants(-200);
+		toulouse.setNbInhabitants(470000);
 		System.out.println(toulouse.getNbInhabitants());
 
 		System.out.println("Ville: " + toulouse.getName() + ", Pays: " + toulouse.getCountry() + ", Nb. d'habitants: "
 				+ toulouse.getNbInhabitants() + ".");
+
+		toulouse.changeProgressHabitants(-100000);
+		System.out.println("Ville: " + toulouse.getName() + ", Pays: " + toulouse.getCountry() + ", Nb. d'habitants: "
+				+ toulouse.getNbInhabitants() + ".");
+
+		/** Polymorphism test **/
+		City rabatWithoutCountry = new City("Rabat", 577000);
+		City rabat = new City("Rabat", "Maroc", 577000);
+
+		System.out.println("[ville : " + rabatWithoutCountry.getName() + "] [pays : " + rabatWithoutCountry.getCountry()
+				+ "] [nombre d'habitants : " + rabatWithoutCountry.getNbInhabitants() + "]");
+		System.out.println("[ville : " + rabat.getName() + "] [pays : " + rabat.getCountry()
+				+ "] [nombre d'habitants : " + rabat.getNbInhabitants() + "]");
 	}
 }
