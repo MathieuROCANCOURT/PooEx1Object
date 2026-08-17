ebb3d802be3fe6c076b6d891d156cad0d4240605
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-17 11:11:03 +0200

    chore: Add contructor, getteurs and setters.

--- POOEx1Object/src/City.java
+++ POOEx1Object/src/City.java
@@ -6,13 +6,67 @@
  * 
  */
 public class City {
+	private String name;
+	private String country;
+	private int nbInhabitants;
+
+	/**
+	 * @param name
+	 * @param country
+	 * @param nbInhabitants
+	 */
+	public City(String name, String country, int nbInhabitants) {
+		this.name = name;
+		this.country = country;
+		this.nbInhabitants = nbInhabitants;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
+
+	/**
+	 * @param country the country to set
+	 */
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	/**
+	 * @return the nbInhabitants
+	 */
+	public int getNbInhabitants() {
+		return nbInhabitants;
+	}
+
+	/**
+	 * @param nbInhabitants the nbInhabitants to set
+	 */
+	public void setNbInhabitants(int nbInhabitants) {
+		this.nbInhabitants = nbInhabitants;
+	}
+
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
 }
