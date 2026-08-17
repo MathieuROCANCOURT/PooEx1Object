9fb8902aa4761bcd65b05d31313fcef54a57d630
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-17 13:03:49 +0200

    feat: Add multiple contructors.

--- POOEx1Object/src/Person.java
+++ POOEx1Object/src/Person.java
@@ -1,10 +1,47 @@
 /**
  * 
  */
-
-/**
- * 
- */
 public class Person {
+	private String lastName;
+	private String firstName;
+	private String old;
+	private String adress;
 
+	/**
+	 * @param lastName
+	 * @param firstName
+	 * @param old
+	 * @param adress
+	 */
+	public Person(String lastName, String firstName, String old, String adress) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.old = old;
+		this.adress = adress;
+	}
+
+	/**
+	 * @param lastName
+	 * @param firstName
+	 * @param old
+	 */
+	public Person(String lastName, String firstName, String old) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.old = old;
+		this.adress = "unknown";
+	}
+
+	/**
+	 * @param lastName
+	 * @param firstName
+	 */
+	public Person(String lastName, String firstName) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.old = "unknown";
+		this.adress = "unknown";
+	}
+
+	
 }
